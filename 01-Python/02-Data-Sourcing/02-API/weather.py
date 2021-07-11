cf5bec2c03d6bb35eb27be08fa8778e224e56c73
Gaetan Manchon <gmanchon@gmail.com>
2021-07-11 17:03:34 +0200

    challengify

--- 01-Python/02-Data-Sourcing/02-API/weather.py
+++ 01-Python/02-Data-Sourcing/02-API/weather.py
@@ -1,4 +1,4 @@
-# pylint: disable=missing-docstring
+# pylint: disable=missing-module-docstring
 
 import sys
 import urllib.parse
@@ -8,20 +8,19 @@ BASE_URI = "https://www.metaweather.com"
 
 
 def search_city(query):
-    # TODO: Look for a given city and disambiguate between several candidates. Return one city (or None)
-    pass
-
+    '''Look for a given city and disambiguate between several candidates. Return one city (or None)'''
+    pass  # YOUR CODE HERE
 
 def weather_forecast(woeid):
-    # TODO: Return a 5-element list of weather forecast for a given woeid
-    pass
-
+    '''Return a 5-element list of weather forecast for a given woeid'''
+    pass  # YOUR CODE HERE
 
 def main():
+    '''Ask user for a city and display weather forecast'''
     query = input("City?\n> ")
     city = search_city(query)
     # TODO: Display weather forecast for a given city
-
+    pass  # YOUR CODE HERE
 
 if __name__ == '__main__':
     try:
--- 01-Python/02-Data-Sourcing/Optional-01-Olympic-Winter-Games/winter_olympic_games.py
+++ 01-Python/02-Data-Sourcing/Optional-01-Olympic-Winter-Games/winter_olympic_games.py
@@ -8,14 +8,13 @@ MEDALS_FILEPATH = "data/winter.csv"
 
 def most_decorated_athlete_ever():
     """Returns who won the most winter olympic games medals (gold/silver/bronze) ever"""
-    pass
+    pass  # YOUR CODE HERE
 
 
 def country_with_most_gold_medals(min_year, max_year):
     """Returns which country won the most gold medals between `min_year` and `max_year`"""
-    pass
-
+    pass  # YOUR CODE HERE
 
 def top_three_women_in_five_thousand_meters():
     """Returns the three women with the most 5000 meters medals(gold/silver/bronze)"""
-    pass
+    pass  # YOUR CODE HERE
