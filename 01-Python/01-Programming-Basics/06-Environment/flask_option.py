d9707425d05f26882ab906ea516de17ba02a96c5
Selma Lopez <selma-lopez@hotmail.fr>
2021-10-05 15:59:06 +0200

    flask env

--- 01-Python/01-Programming-Basics/06-Environment/flask_option.py
+++ 01-Python/01-Programming-Basics/06-Environment/flask_option.py
@@ -4,7 +4,11 @@ import os
 
 def start():
     """returns the right message"""
-    pass  # YOUR CODE HERE
+    flask_env = os.getenv('FLASK_ENV')
+    if flask_env:
+        return "Starting in development mode..."
+    else:
+        return "Starting in production mode..."
 
 if __name__ == "__main__":
     print(start())
