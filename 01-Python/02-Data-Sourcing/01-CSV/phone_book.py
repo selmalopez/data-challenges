73e5838b8a1c93627a54135d4762b13afb1a7dbd
Selma Lopez <selma-lopez@hotmail.fr>
2021-10-06 11:28:58 +0200

    day 3- CVS

--- 01-Python/02-Data-Sourcing/01-CSV/phone_book.py
+++ 01-Python/02-Data-Sourcing/01-CSV/phone_book.py
@@ -1,1 +1,9 @@
+import csv
 
+with open('data/phone_book.csv', mode='r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    #line_count = 0
+    for row in csv_reader:
+        #if line_count > 0:
+        print(row['last_name'], int(row['phone_number']))
+        #line_count += 1
