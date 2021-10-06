80351d3e4ad0248d405498e01f8357f35f98d19e
Selma Lopez <selma-lopez@hotmail.fr>
2021-10-06 11:31:17 +0200

    DAY 3 -CVS

--- 01-Python/02-Data-Sourcing/01-CSV/phone_book.py
+++ 01-Python/02-Data-Sourcing/01-CSV/phone_book.py
@@ -1,9 +1,12 @@
 import csv
 
 with open('data/phone_book.csv', mode='r') as csv_file:
-    csv_reader = csv.DictReader(csv_file)
-    #line_count = 0
+    csv_reader = csv.reader(csv_file)
+    line_count = 0
     for row in csv_reader:
-        #if line_count > 0:
-        print(row['last_name'], int(row['phone_number']))
-        #line_count += 1
+        #print(row[0],row[2])
+
+        if line_count > 0:
+            print(row[0], row[2])
+
+        line_count += 1
