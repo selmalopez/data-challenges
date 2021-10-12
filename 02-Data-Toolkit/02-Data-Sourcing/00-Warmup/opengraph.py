81eda081af7ba0cc2ec6764d9e630d6556f84ceb
Selma Lopez <selma-lopez@hotmail.fr>
2021-10-12 11:37:12 +0200

    warm up

--- 02-Data-Toolkit/02-Data-Sourcing/00-Warmup/opengraph.py
+++ 02-Data-Toolkit/02-Data-Sourcing/00-Warmup/opengraph.py
@@ -2,14 +2,25 @@
 """
 Client of the Wagon OpenGraph API
 """
-
+import pprint
 import requests
+#pp = pprint.PrettyPrinter(indent=4)
+#pp.pprint(fetch_metadata("https://www.lewagon.com"))
 
 def fetch_metadata(url):
     """
-    Return a dictionary of OpenGraph metadata found in HTML of given url
     """
-    pass # TODO: implement this function!
+    
+    response = requests.get(f'https://opengraph.lewagon.com/?url={url}')
+    if response.status_code != 200:
+        return ''
+    data = response.json()['data']
+    
+    return data
+    
+ 
+    #Return a dictionary of OpenGraph metadata found in HTML of given url
+    # TODO: implement this function!
 
 # To manually test, please uncomment the following lines and run `python opengraph.py`:
 # import pprint
