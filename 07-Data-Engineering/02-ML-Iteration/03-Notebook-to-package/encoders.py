23504c4bab08557d412e385ad79d58e14dbf2d80
Selma Lopez <selma-lopez@hotmail.fr>
2021-11-16 14:46:36 +0100

    update

--- 07-Data-Engineering/02-ML-Iteration/03-Notebook-to-package/encoders.py
+++ 07-Data-Engineering/02-ML-Iteration/03-Notebook-to-package/encoders.py
@@ -1,12 +1,27 @@
 from sklearn.base import BaseEstimator, TransformerMixin
-
+import pandas as pd
 
 class TimeFeaturesEncoder(BaseEstimator, TransformerMixin):
     """Extract the day of week (dow), the hour, the month and the year from a
     time column."""
-    pass
+    def __init__(self, time_column, time_zone_name='America/New_York'):
+        self.time_column = time_column
+        self.time_zone_name = time_zone_name
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        assert isinstance(X, pd.DataFrame)
+        X_ = X.copy()
+        X_.index = pd.to_datetime(X[self.time_column])
+        X_.index = X_.index.tz_convert(self.time_zone_name)
+        X_["dow"] = X_.index.weekday
+        X_["hour"] = X_.index.hour
+        X_["month"] = X_.index.month
+        X_["year"] = X_.index.year
+        return X_[['dow', 'hour', 'month', 'year']]
 
 
 class DistanceTransformer(BaseEstimator, TransformerMixin):
     """Compute the haversine distance between two GPS points."""
-    pass
--- 07-Data-Engineering/02-ML-Iteration/03-Notebook-to-package/trainer.py
+++ 07-Data-Engineering/02-ML-Iteration/03-Notebook-to-package/trainer.py
@@ -1,4 +1,14 @@
 # imports
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_squared_error
+from utils import *
+from encoders import *
+
 
 class Trainer():
     def __init__(self, X, y):
@@ -12,15 +22,38 @@ class Trainer():
 
     def set_pipeline(self):
         """defines the pipeline as a class attribute"""
-        pass
+
+        dist_pipe = Pipeline([
+        ('dist_trans', DistanceTransformer()),
+        ('stdscaler', StandardScaler())
+        ])
+        time_pipe = Pipeline([
+        ('time_enc', TimeFeaturesEncoder('pickup_datetime')),
+        ('ohe', OneHotEncoder(handle_unknown='ignore'))
+        ])
+        preproc_pipe = ColumnTransformer([
+        ('distance', dist_pipe, ["pickup_latitude", "pickup_longitude", 'dropoff_latitude', 'dropoff_longitude']),
+        ('time', time_pipe, ['pickup_datetime'])
+        ], remainder="drop")
+
+        pipe = Pipeline([
+        ('preproc', preproc_pipe),
+        ('linear_model', LinearRegression())
+        ])
+        return pipe
+
 
     def run(self):
         """set and train the pipeline"""
-        pass
+        return self.set_pipeline(self.X,self.y).fit
+
+
 
     def evaluate(self, X_test, y_test):
         """evaluates the pipeline on df_test and return the RMSE"""
-        pass
+        y_pred = self.pipeline.predict(X_test)
+        return compute_rmse(y_pred, y_test)
+
 
 
 if __name__ == "__main__":
